3fa349d2f647c2db6c5852a5fbbaca500b29e8f6
Arzu Caner <arzuguneycaner@gmail.com>
2023-12-21 22:45:22 +0000

    Add second project

--- src/pages/ProjectsPage.jsx
+++ src/pages/ProjectsPage.jsx
@@ -1,11 +1,14 @@
 import React from 'react';
 import projectImage from '../assets/project-1.png';
+import projectImage2 from '../assets/portfolio-1.png';
+
 
 const ProjectPage = () => {
   const containerStyle = {
     textAlign: 'center',
     fontFamily: 'Quantico, sans-serif',
     padding:'20px',
+    justifyContent: 'space-around',
   };
 
   const imageStyle = {
@@ -18,38 +21,29 @@ const ProjectPage = () => {
     <div style={containerStyle}>
       <h2>React TriviaQuest Challenge</h2>
       <img src={projectImage} alt="project" style={imageStyle} />
-      <p>
-        React TriviaQuest Challenge is an interactive trivia quiz application developed using React. This project provides users with a fun experience to test their general knowledge and information.
-      </p>
-
-      <h3>Used Technologies</h3>
-      <ul>
-        <li>React.js</li>
-        <li>API</li>
-      </ul>
-
-      <h3>Achievements and Challenges</h3>
-      <p>
-        A brief assessment of achievements obtained and challenges faced
-        during the project is provided.
-      </p>
-
-      <h3>Links</h3>
-      <ul>
-        <li>
-          <a href="https://github.com/arzucaner/react-triviaquest-challenge" target="_blank" rel="noopener noreferrer">
-            GitHub Repo
-          </a>
-        </li>
-        <li>
-          <a href="https://www.youtube.com/watch?v=ViAYjc-SEtA&t=9s" target="_blank" rel="noopener noreferrer">
-            Live Demo
-          </a>
-        </li>
-      </ul>
+      description="React TriviaQuest Challenge is an interactive trivia quiz application developed using React. This project provides users with a fun experience to test their general knowledge and information."
+        technologies={['React.js', 'API']}
+        achievementsAndChallenges="A brief assessment of achievements obtained and challenges faced during the project is provided."
+        links={[
+          { text: 'GitHub Repo', url: 'https://github.com/arzucaner/react-triviaquest-challenge' },
+          { text: 'Live Demo', url: 'https://www.youtube.com/watch?v=ViAYjc-SEtA&t=9s' },
+        ]}
+      />
+      
+      <Project
+        title="Travel Brew"
+        image={projectImage2}
+        description="TravelBrew is a mobile application designed to assist travellers in discovering the best coffee and cosy spots in the places they visit. The app empowers users to explore top-notch venues that cater to their essential needs, such as coffee, sandwiches, seating areas, and restrooms, while they travel through different cities."
+        technologies={['React Native', 'Google API']}
+        achievementsAndChallenges="3rd Place Winner."
+        links={[
+          { text: 'GitHub Repo', url: 'https://github.com/arzucaner/travel-brew' },
+          { text: 'Live Demo', url: 'https://www.youtube.com/watch?v=A1DmeX5OwPI' },
+        ]}
+      />
     </div>
   );
 };
 
 export default ProjectPage;
-
+      
